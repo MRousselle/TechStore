2bc72c7cb16fda9964b3d954f7463d80c5ca6520
Tyler <tylernicol556@gmail.com>
2015-12-10 17:36:37 -0500

    added php registration files
    connectdb.php
    reg.php (register handler)
    registration.php (registration form)

---
+++ registration.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Butts</title>
+</head>
+<body>
+    <form action="reg.php" method="post" name="" id="">
+        First Name: <input type="text" name="firstName" id="firstName" required><br>
+        Last Name: <input type="text" name="lastName" id="lastName" required><br>
+        Email: <input type="email" name="email" id="email"><br>
+        User Name: <input type="text" name="userName" id="userName" required><br>
+        Password: <input type="text" name="password" id="password" required><br>
+        <input type="submit" value="Register">
+    </form>
+</body>
+</html>
