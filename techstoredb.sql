c7ff17ee15f9102f3439a341b96f8d94a975bcf8
Tyler <tylernicol556@gmail.com>
2015-12-10 16:57:38 -0500

    TechStore DB Creation
    added the sql code for creating the database

---
+++ techstoredb.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.0.2
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 10, 2015 at 10:53 PM
+-- Server version: 10.0.17-MariaDB
+-- PHP Version: 5.6.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `techstore`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `prod_name` varchar(20) NOT NULL,
+  `prod_desc` varchar(50) NOT NULL,
+  `prod_img` varchar(20) NOT NULL,
+  `prod_price` decimal(10,0) NOT NULL,
+  `prod_qty` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_ID` int(11) NOT NULL,
+  `user_firstName` varchar(20) NOT NULL,
+  `user_lastName` varchar(20) NOT NULL,
+  `user_email` varchar(30) NOT NULL,
+  `user_userName` varchar(20) NOT NULL,
+  `user_password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_ID`),
+  ADD UNIQUE KEY `user_ID` (`user_ID`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
