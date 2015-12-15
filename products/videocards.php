68e5e4b028274a14d039b57319c88079d62220f2
tylernicol <n00687086@L159-N-023.mediastudies.ca>
2015-12-15 10:53:45 -0500

    Product page revamp
    Next Step, adding huge amounts of data to the data base

--- products/videocards.php
+++ products/videocards.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <title>Catalog</title>
+    <link rel="stylesheet" href="../normalize.css">
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,500,700|Oswald' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="../style.css">
 </head>
 <body>
     <?php include "../connectdb.php" ?>
@@ -10,22 +13,37 @@
         $results = mysql_query("SELECT * FROM products WHERE prod_cat = 'videocards'") or die ("retrieving info failed: ").mysql_error;
         $num_rows = mysql_num_rows($results);
     ?>
-    <h2>List of Products</h2>
-    <table border="2">
-        <tr>
-            <td>Category Name</td>
-            <td>Category Description</td>
-            <td>Category Image</td>
-        </tr>
+    <div class="wrapper">
+    <div class="greenBack">
+    <header>
+        <h2>RJTS Computers</h2>
+        <nav>
+            <ul>
+                <a href="../categories.php"><li>Home</li></a>
+                <a href=""><li>Cart</li></a>
+                <a href=""><li>Log Out</li></a>
+            </ul>
+        </nav>
+    </header>
+    <table>
         <?php
-            if ($num_rows = 0) {
-//                echo "No Products, check back soon";
+            if ($num_rows == 0) {
+               echo "No Products, check back soon";
             } else {
                 while($row = mysql_fetch_array($results)){
-                    echo'<tr><td> '.$row['prod_name'].'</td><td>'.$row['prod_desc'].'</td><td><img src="images/'.$row['prod_img'].'"/></td></tr>';
+                    echo'<tr class="productTR">
+                            <td><img src="../images/'.$row['prod_img'].'"/></td>
+                            <td>
+                                <span class="prodName">'.$row['prod_name'].'</span>
+                                '.$row['prod_desc'].'
+                            </td>
+                            <td>$'.$row['prod_price'].'</td>
+                        </tr>';
                 }
             }
         ?>
     </table>
+    </div>
+    </div>
 </body>
 </html>
