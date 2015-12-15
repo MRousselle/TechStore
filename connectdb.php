9529e7f648f661786e6fc999473adb8506f26ef5
tylernicol <n00687086@L159-N-023.mediastudies.ca>
2015-12-15 09:45:08 -0500

    refreshing integrity

--- connectdb.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $link = mysql_connect("localhost", "root", "") or die (mysql_error());
-    $db = "techstore";
-    if (! $link) die ("Could not connect to MySQL");
-    mysql_select_db($db, $link) or die ("Select DB Error:".mysql_error());
-?>
